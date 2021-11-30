274cfcc95be09c4661102d372050b226053081e1
Oscar <fu.wire@gmail.com>
2021-11-30 14:04:49 -0600

    chore: update script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -3,6 +3,12 @@
 END=52
 
 for i in $(seq $END); do
+  echo "=== PROCESSING DAY : $i ==="
   curl -s https://content.benji.org/days?day_eq=$i > content/day/${i}.json
   cat content/day/${i}.json
+  echo "=== FINISHED DAY: ${i} ==="
 done
+
+echo "Leaving with"
+
+ls -ltah content/day
